e38d0050f59be916e0ffefb63936616a62280841
Paolo <pieffelleo@gmail.com>
2022-02-19 15:12:17 +0100

    type game app - angular binding input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Type_game';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomLetter: string, enteredTextValue: string) {
+    if (!enteredTextValue) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredTextValue ? 'correct' : 'incorrect';
+  }
 }
